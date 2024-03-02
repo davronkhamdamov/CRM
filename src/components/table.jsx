e342a0c5dae0fc87c5d2982961da58c1216c70f4
davronkhamdamov <xamdamovdavron6@gmail.com>
2024-03-02 20:03:16 +0500

    added theme mode

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -4,159 +4,158 @@ import { useEffect, useState } from "react";
 import { DownOutlined, LoadingOutlined } from "@ant-design/icons";
 
 const actionItems = [
-  {
-    key: "1",
-    label: "Action 1",
-  },
-  {
-    key: "2",
-    label: "Action 2",
-  },
+    {
+        key: "1",
+        label: "Action 1",
+    },
+    {
+        key: "2",
+        label: "Action 2",
+    },
 ];
 const TableComponent = () => {
-  const [form] = Form.useForm();
-  const [messageApi, contextHolder] = message.useMessage();
+    const [messageApi, contextHolder] = message.useMessage();
 
-  const cancel = () => {
-    messageApi
-      .open({
-        type: "loading",
-        content: "Jarayonda...",
-        duration: 2.5,
-      })
-      .then(() => message.success("Muvaffaqqiyatli o'chirildi", 2.5));
-  };
+    const cancel = () => {
+        messageApi
+            .open({
+                type: "loading",
+                content: "Jarayonda...",
+                duration: 2.5,
+            })
+            .then(() => message.success("Muvaffaqqiyatli o'chirildi", 2.5));
+    };
 
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      sorter: true,
-      render: (name) => `${name.first} ${name.last}`,
-      width: "20%",
-    },
-    {
-      title: "Gender",
-      dataIndex: "gender",
-      filters: [
+    const columns = [
         {
-          text: "Male",
-          value: "male",
+            title: "Name",
+            dataIndex: "name",
+            sorter: true,
+            render: (name) => `${name.first} ${name.last}`,
+            width: "20%",
         },
         {
-          text: "Female",
-          value: "female",
+            title: "Gender",
+            dataIndex: "gender",
+            filters: [
+                {
+                    text: "Male",
+                    value: "male",
+                },
+                {
+                    text: "Female",
+                    value: "female",
+                },
+            ],
+            width: "20%",
         },
-      ],
-      width: "20%",
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-    },
-    {
-      title: "Action",
-      dataIndex: "operation",
-      key: "operation",
-      render: () => {
-        return (
-          <Space size="middle">
-            <Popconfirm
-              title="O'chirishga ishonchingiz komilmi?"
-              onConfirm={cancel}
-            >
-              <a>O'chirish</a>
-            </Popconfirm>
-            <a>Stop</a>
-            <Dropdown
-              menu={{
-                items: actionItems,
-              }}
-            >
-              <a>
-                More <DownOutlined />
-              </a>
-            </Dropdown>
-          </Space>
-        );
-      },
-    },
-  ];
+        {
+            title: "Email",
+            dataIndex: "email",
+        },
+        {
+            title: "Action",
+            dataIndex: "operation",
+            key: "operation",
+            render: () => {
+                return (
+                    <Space size="middle">
+                        <Popconfirm
+                            title="O'chirishga ishonchingiz komilmi?"
+                            onConfirm={cancel}
+                        >
+                            <a>O'chirish</a>
+                        </Popconfirm>
+                        <a>Tahrirlash</a>
+                        <Dropdown
+                            menu={{
+                                items: actionItems,
+                            }}
+                        >
+                            <a>
+                                More <DownOutlined />
+                            </a>
+                        </Dropdown>
+                    </Space>
+                );
+            },
+        },
+    ];
 
-  const getRandomuserParams = (params) => ({
-    results: params.pagination?.pageSize,
-    page: params.pagination?.current,
-    ...params,
-  });
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(false);
-  const [tableParams, setTableParams] = useState({
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
-  });
-  const fetchData = () => {
-    setLoading(true);
-    fetch(
-      `https://randomuser.me/api?${qs.stringify(
-        getRandomuserParams(tableParams)
-      )}`
-    )
-      .then((res) => res.json())
-      .then(({ results }) => {
-        setData(results);
-        setLoading(false);
-        setTableParams({
-          ...tableParams,
-          pagination: {
-            ...tableParams.pagination,
-            total: 200,
-          },
-        });
-      });
-  };
-  useEffect(() => {
-    fetchData();
-  }, [JSON.stringify(tableParams)]);
-  const handleTableChange = (pagination, filters, sorter) => {
-    setTableParams({
-      pagination,
-      filters,
-      ...sorter,
+    const getRandomuserParams = (params) => ({
+        results: params.pagination?.pageSize,
+        page: params.pagination?.current,
+        ...params,
     });
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(false);
+    const [tableParams, setTableParams] = useState({
+        pagination: {
+            current: 1,
+            pageSize: 10,
+        },
+    });
+    const fetchData = () => {
+        setLoading(true);
+        fetch(
+            `https://randomuser.me/api?${qs.stringify(
+                getRandomuserParams(tableParams)
+            )}`
+        )
+            .then((res) => res.json())
+            .then(({ results }) => {
+                setData(results);
+                setLoading(false);
+                setTableParams({
+                    ...tableParams,
+                    pagination: {
+                        ...tableParams.pagination,
+                        total: 200,
+                    },
+                });
+            });
+    };
+    useEffect(() => {
+        fetchData();
+    }, [JSON.stringify(tableParams)]);
+    const handleTableChange = (pagination, filters, sorter) => {
+        setTableParams({
+            pagination,
+            filters,
+            ...sorter,
+        });
 
-    if (pagination.pageSize !== tableParams.pagination?.pageSize) {
-      setData([]);
-    }
-  };
-
-  return (
-    <Form form={form} component={false}>
-      <Table
-        columns={columns}
-        rowKey={(record) => record.login.uuid}
-        dataSource={data}
-        pagination={tableParams.pagination}
-        loading={
-          loading
-            ? {
-                indicator: (
-                  <LoadingOutlined
-                    style={{
-                      fontSize: 34,
-                    }}
-                    spin
-                  />
-                ),
-              }
-            : false
+        if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+            setData([]);
         }
-        onChange={handleTableChange}
-      />
-      {contextHolder}
-    </Form>
-  );
+    };
+
+    return (
+        <>
+            <Table
+                columns={columns}
+                rowKey={(record) => record.login.uuid}
+                dataSource={data}
+                pagination={tableParams.pagination}
+                loading={
+                    loading
+                        ? {
+                              indicator: (
+                                  <LoadingOutlined
+                                      style={{
+                                          fontSize: 34,
+                                      }}
+                                      spin
+                                  />
+                              ),
+                          }
+                        : false
+                }
+                onChange={handleTableChange}
+            />
+            {contextHolder}
+        </>
+    );
 };
 
 export default TableComponent;
