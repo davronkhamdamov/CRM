64df6559645e0e50556bf0e06fcb276aa263cf9c
davronkhandamov <xamdamovdavron6@gmail.com>
2024-03-10 17:03:15 +0500

    added loader payment type

--- src/components/CreatePaymentType.tsx
+++ src/components/CreatePaymentType.tsx
@@ -1,68 +1,81 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
-import { Button, Drawer, Form, Input, Space } from "antd";
+import { Button, Drawer, Form, Input, Space, message } from "antd";
+import { LoadingProvider } from "../App";
 
 const CreatePaymentType = () => {
-    const [open, setOpen] = useState(false);
-    const showDrawer = () => {
-        setOpen(true);
-    };
-    const onClose = () => {
-        setOpen(false);
-    };
-    // const onChange = (value: string) => {
-    //     console.log(`selected ${value}`);
-    // };
+  const { setLoadingCnx } = useContext(LoadingProvider);
+  const [messageApi, contextHolder] = message.useMessage();
 
-    // const onSearch = (value: string) => {
-    //     console.log("search:", value);
-    // };
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  const onSubmit = () => {
+    setLoadingCnx(true);
+    setTimeout(() => {
+      setOpen(false);
+      setLoadingCnx(false);
+      messageApi.success("To'lov turi muvaffaqqiyatli yaratildi", 2);
+    }, 2000);
+  };
+  // const onChange = (value: string) => {
+  //     console.log(`selected ${value}`);
+  // };
 
-    // const filterOption = (
-    //     input: string,
-    //     option?: { label: string; value: string }
-    // ) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
+  // const onSearch = (value: string) => {
+  //     console.log("search:", value);
+  // };
 
-    return (
-        <>
-            <Button type="default" onClick={showDrawer} icon={<PlusOutlined />}>
-                Yangi to'lov turini qoshish
+  // const filterOption = (
+  //     input: string,
+  //     option?: { label: string; value: string }
+  // ) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
+
+  return (
+    <>
+      {contextHolder}
+      <Button type="default" onClick={showDrawer} icon={<PlusOutlined />}>
+        Yangi to'lov turini qoshish
+      </Button>
+      <Drawer
+        title="Yangi to'lov turini qo'shish"
+        width={500}
+        onClose={onClose}
+        open={open}
+        styles={{
+          body: {
+            paddingBottom: 80,
+          },
+        }}
+        extra={
+          <Space>
+            <Button onClick={onClose}>Bekor qilish</Button>
+            <Button onClick={onSubmit} type="primary">
+              Yaratish
             </Button>
-            <Drawer
-                title="Yangi to'lov turini qo'shish"
-                width={500}
-                onClose={onClose}
-                open={open}
-                styles={{
-                    body: {
-                        paddingBottom: 80,
-                    },
-                }}
-                extra={
-                    <Space>
-                        <Button onClick={onClose}>Bekor qilish</Button>
-                        <Button onClick={onClose} type="primary">
-                            Yaratish
-                        </Button>
-                    </Space>
-                }
-            >
-                <Form layout="vertical">
-                    <Form.Item
-                        name="first_name"
-                        label="To'lov turi"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Iltimos to'lov turi nomini kiriting",
-                            },
-                        ]}
-                    >
-                        <Input placeholder="Iltimos to'lov turi nomini kiriting" />
-                    </Form.Item>
-                </Form>
-            </Drawer>
-        </>
-    );
+          </Space>
+        }
+      >
+        <Form layout="vertical">
+          <Form.Item
+            name="first_name"
+            label="To'lov turi"
+            rules={[
+              {
+                required: true,
+                message: "Iltimos to'lov turi nomini kiriting",
+              },
+            ]}
+          >
+            <Input placeholder="Iltimos to'lov turi nomini kiriting" />
+          </Form.Item>
+        </Form>
+      </Drawer>
+    </>
+  );
 };
 export default CreatePaymentType;
