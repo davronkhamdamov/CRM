2e4698a01a7713ed7cd65057ebae9c62bb3687da
davronkhandamov <xamdamovdavron6@gmail.com>
2024-03-10 16:57:53 +0500

    added not found page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+const NotFoundPage = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Not found</h1>
+    </div>
+  );
+};
+export default NotFoundPage;
--- src/router/route.tsx
+++ src/router/route.tsx
@@ -1,7 +1,7 @@
 import {
-    Route,
-    createBrowserRouter,
-    createRoutesFromElements,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
 } from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
 import Bemorlar from "../pages/Bemorlar";
@@ -20,42 +20,44 @@ import Settings from "../pages/Settings";
 import Hisobot from "../pages/Hisobot";
 import DockerLayout from "../layout/DoctorLayout";
 import ForgetPassword from "../pages/ForgetPassword";
+import NotFoundPage from "../pages/NotFoundPage";
 
 const route = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route path="/admin" element={<RootLayout />}>
-                <Route index path="statistic" element={<Statistic />} />
-                <Route path="payment" element={<Payment />} />
-                <Route path="patient" element={<Bemorlar />} />
-                <Route path="patient/:id" element={<SingleBemor />} />
-                <Route path="doctors" element={<Doctors />} />
-                <Route path="doctors/:id" element={<SingleDocktor />} />
-                <Route path="payment-type" element={<PaymentTypes />} />
-                <Route path="services" element={<Services />} />
-                <Route path="treatment" element={<Treatment />} />
-                <Route path="settings" element={<Settings />} />
-                <Route path="hisobot" element={<Hisobot />} />
-            </Route>
-            <Route path="doctor" element={<DockerLayout />}>
-                <Route index path="statistic" element={<Statistic />} />
-                <Route path="treatment" element={<Treatment />} />
-            </Route>
-            <Route path="/reception" element={<Reception />}>
-                <Route path="payment" element={<Payment />} />
-                <Route index path="statistic" element={<Statistic />} />
-                <Route path="patient" element={<Bemorlar />} />
-                <Route path="patient/:id" element={<SingleBemor />} />
-                <Route path="treatment" element={<Treatment />} />
-            </Route>
+  createRoutesFromElements(
+    <>
+      <Route path="/admin" element={<RootLayout />}>
+        <Route index path="statistic" element={<Statistic />} />
+        <Route path="payment" element={<Payment />} />
+        <Route path="patient" element={<Bemorlar />} />
+        <Route path="patient/:id" element={<SingleBemor />} />
+        <Route path="doctors" element={<Doctors />} />
+        <Route path="doctors/:id" element={<SingleDocktor />} />
+        <Route path="payment-type" element={<PaymentTypes />} />
+        <Route path="services" element={<Services />} />
+        <Route path="treatment" element={<Treatment />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="hisobot" element={<Hisobot />} />
+      </Route>
+      <Route path="doctor" element={<DockerLayout />}>
+        <Route index path="statistic" element={<Statistic />} />
+        <Route path="treatment" element={<Treatment />} />
+      </Route>
+      <Route path="/reception" element={<Reception />}>
+        <Route path="payment" element={<Payment />} />
+        <Route index path="statistic" element={<Statistic />} />
+        <Route path="patient" element={<Bemorlar />} />
+        <Route path="patient/:id" element={<SingleBemor />} />
+        <Route path="treatment" element={<Treatment />} />
+      </Route>
 
-            <Route path="/auth">
-                <Route index element={<Login />} />
-                <Route path="forget-password" element={<ForgetPassword />} />
-            </Route>
-            <Route path="/book" element={<Book />} />
-        </>
-    )
+      <Route path="/auth">
+        <Route index element={<Login />} />
+        <Route path="forget-password" element={<ForgetPassword />} />
+      </Route>
+      <Route path="/book" element={<Book />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </>
+  )
 );
 
 export default route;
