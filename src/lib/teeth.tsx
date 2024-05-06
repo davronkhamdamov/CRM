072c3f18e38fdade478750340a13d974ca254185
davronkhandamov <xamdamovdavron6@gmail.com>
2024-05-06 22:48:57 +0500

    update teeth layout

--- src/lib/teeth.tsx
+++ src/lib/teeth.tsx
@@ -216,14 +216,14 @@ const teeth = [
     img: eleven,
     style: {
       width: "60px",
-      transform: "translate(-140px, -5px)",
+      transform: "translate(0, -5px)",
     },
     text_style: {
-      transform: "translate(-60px, -20px)",
+      transform: "translate(70px, -20px)",
     },
     selected: false,
     div_style: {
-      left: "310px",
+      left: "180px",
       top: "135px",
       width: "50px",
       height: "45px",
@@ -234,14 +234,14 @@ const teeth = [
     img: twelve,
     style: {
       width: "60px",
-      transform: "translate(-120px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-40px, -10px)",
+      transform: "translate(80px, -10px)",
     },
     selected: false,
     div_style: {
-      left: "330px",
+      left: "220px",
       top: "180px",
       width: "60px",
       height: "50px",
@@ -252,14 +252,14 @@ const teeth = [
     img: thirteen,
     style: {
       width: "60px",
-      transform: "translate(-110px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-30px, -5px)",
+      transform: "translate(80px, -5px)",
     },
     selected: false,
     div_style: {
-      left: "345px",
+      left: "245px",
       top: "230px",
       width: "55px",
       height: "45px",
@@ -270,14 +270,14 @@ const teeth = [
     img: fourteen,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-20px, 10px)",
+      transform: "translate(80px, 10px)",
     },
     selected: false,
     div_style: {
-      left: "355px",
+      left: "275px",
       top: "275px",
       width: "60px",
       height: "65px",
@@ -288,14 +288,14 @@ const teeth = [
     img: fifteen,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-10px, 15px)",
+      transform: "translate(80px, 15px)",
     },
     selected: false,
     div_style: {
-      left: "370px",
+      left: "290px",
       top: "340px",
       width: "50px",
       height: "60px",
@@ -306,14 +306,14 @@ const teeth = [
     img: sixteen,
     style: {
       width: "65px",
-      transform: "translate(-90px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(0, 10px)",
+      transform: "translate(80px, 10px)",
     },
     selected: false,
     div_style: {
-      left: "375px",
+      left: "295px",
       top: "400px",
       width: "55px",
       height: "55px",
@@ -324,14 +324,14 @@ const teeth = [
     img: seventeen,
     style: {
       width: "65px",
-      transform: "translate(-90px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(5px, 20px)",
+      transform: "translate(80px, 20px)",
     },
     selected: false,
     div_style: {
-      left: "370px",
+      left: "290px",
       top: "485px",
       width: "60px",
       height: "64px",
@@ -342,14 +342,14 @@ const teeth = [
     img: eighteen,
     style: {
       width: "60px",
-      transform: "translate(-90px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(0, 20px)",
+      transform: "translate(80px, 20px)",
     },
     selected: false,
     div_style: {
-      left: "365px",
+      left: "285px",
       top: "555px",
       width: "65px",
       height: "64px",
@@ -360,14 +360,14 @@ const teeth = [
     img: nineteen,
     style: {
       width: "65px",
-      transform: "translate(-95px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-10px, 30px)",
+      transform: "translate(80px, 30px)",
     },
     selected: false,
     div_style: {
-      left: "355px",
+      left: "270px",
       top: "625px",
       width: "70px",
       height: "70px",
@@ -378,14 +378,14 @@ const teeth = [
     img: twenty,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-30px, 30px)",
+      transform: "translate(70px, 30px)",
     },
     selected: false,
     div_style: {
-      left: "350px",
+      left: "250px",
       top: "695px",
       width: "50px",
       height: "50px",
@@ -396,14 +396,14 @@ const teeth = [
     img: twentyone,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-30px, 40px)",
+      transform: "translate(65px, 35px)",
     },
     selected: false,
     div_style: {
-      left: "330px",
+      left: "230px",
       top: "745px",
       width: "50px",
       height: "45px",
@@ -414,14 +414,14 @@ const teeth = [
     img: twentytwo,
     style: {
       width: "45px",
-      transform: "translate(-120px, -10px)",
+      transform: "translate(0, -10px)",
     },
     text_style: {
-      transform: "translate(-70px, 45px)",
+      transform: "translate(50px, 45px)",
     },
     selected: false,
     div_style: {
-      left: "320px",
+      left: "200px",
       top: "790px",
       width: "40px",
       height: "50px",
@@ -432,14 +432,14 @@ const teeth = [
     img: twentythree,
     style: {
       width: "40px",
-      transform: "translate(-118px, 5px)",
+      transform: "translate(0, 5px)",
     },
     text_style: {
-      transform: "translate(-80px, 60px)",
+      transform: "translate(35px, 55px)",
     },
     selected: false,
     div_style: {
-      left: "280px",
+      left: "160px",
       top: "810px",
       width: "35px",
       height: "55px",
@@ -450,14 +450,14 @@ const teeth = [
     img: twentyfour,
     style: {
       width: "40px",
-      transform: "translate(-128px, 10px)",
+      transform: "translate(0, 10px)",
     },
     text_style: {
-      transform: "translate(-100px, 70px)",
+      transform: "translate(30px, 60px)",
     },
     selected: false,
     div_style: {
-      left: "240px",
+      left: "110px",
       top: "815px",
       width: "35px",
       height: "50px",
@@ -468,14 +468,14 @@ const teeth = [
     img: twentyfive,
     style: {
       width: "40px",
-      transform: "translate(-128px, 10px)",
+      transform: "translate(0, 10px)",
     },
     text_style: {
-      transform: "translate(-100px, 70px)",
+      transform: "translate(30px, 60px)",
     },
     selected: false,
     div_style: {
-      left: "195px",
+      left: "65px",
       top: "815px",
       width: "40px",
       height: "50px",
@@ -486,14 +486,14 @@ const teeth = [
     img: twentysix,
     style: {
       width: "40px",
-      transform: "translate(-137px, 10px)",
+      transform: "translate(0, 10px)",
     },
     text_style: {
-      transform: "translate(-115px, 60px)",
+      transform: "translate(20px, 60px)",
     },
     selected: false,
     div_style: {
-      left: "155px",
+      left: "20px",
       top: "810px",
       width: "40px",
       height: "50px",
@@ -504,14 +504,14 @@ const teeth = [
     img: twentyseven,
     style: {
       width: "45px",
-      transform: "translate(-135px, -10px)",
+      transform: "translate(0, -10px)",
     },
     text_style: {
-      transform: "translate(-135px, 45px)",
+      transform: "translate(0, 35px)",
     },
     selected: false,
     div_style: {
-      left: "105px",
+      left: "-25px",
       top: "790px",
       width: "50px",
       height: "50px",
@@ -522,14 +522,14 @@ const teeth = [
     img: twentyeight,
     style: {
       width: "60px",
-      transform: "translate(-145px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-140px, 30px)",
+      transform: "translate(0, 30px)",
     },
     selected: false,
     div_style: {
-      left: "80px",
+      left: "-70px",
       top: "745px",
       height: "45px",
       width: "45px",
@@ -540,14 +540,14 @@ const teeth = [
     img: twentynine,
     style: {
       width: "60px",
-      transform: "translate(-142px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-150px, 30px)",
+      transform: "translate(0, 30px)",
     },
     selected: false,
     div_style: {
-      left: "55px",
+      left: "-90px",
       top: "695px",
       height: "50px",
       width: "53px",
@@ -558,14 +558,14 @@ const teeth = [
     img: thirty,
     style: {
       width: "60px",
-      transform: "translate(-145px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-160px , 40px)",
+      transform: "translate(0 , 40px)",
     },
     selected: false,
     div_style: {
-      left: "40px",
+      left: "-110px",
       top: "625px",
       width: "65px",
       height: "68px",
@@ -576,14 +576,14 @@ const teeth = [
     img: thirtyone,
     style: {
       width: "60px",
-      transform: "translate(-155px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-170px, 45px)",
+      transform: "translate(0, 35px)",
     },
     selected: false,
     div_style: {
-      left: "25px",
+      left: "-135px",
       top: "550px",
       height: "70px",
       width: "65px",
@@ -594,14 +594,14 @@ const teeth = [
     img: thirtytwo,
     style: {
       width: "60px",
-      transform: "translate(-165px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-175px, 25px)",
+      transform: "translate(0, 25px)",
     },
     selected: false,
     div_style: {
-      left: "25px",
+      left: "-140px",
       top: "485px",
       width: "60px",
       height: "60px",
@@ -613,16 +613,16 @@ const teeth = [
     img: one,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-100px, 20px)",
+      transform: "translate(0px, 20px)",
     },
     selected: false,
     div_style: {
       width: "50px",
       height: "50px",
-      left: "50px",
+      left: "-50px",
       top: "400px",
     },
   },
@@ -631,14 +631,14 @@ const teeth = [
     img: two,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-100px, 20px)",
+      transform: "translate(0, 20px)",
     },
     selected: false,
     div_style: {
-      left: "50px",
+      left: "-50px",
       top: "335px",
       width: "60px",
       height: "60px",
@@ -649,14 +649,13 @@ const teeth = [
     img: six,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
     },
     text_style: {
-      transform: "translate(-100px, -10px)",
+      transform: "translate(0, 0)",
     },
     selected: false,
     div_style: {
-      left: "60px",
+      left: "-30px",
       top: "280px",
       width: "55px",
       height: "45px",
@@ -666,15 +665,15 @@ const teeth = [
     id: 52,
     img: seven,
     style: {
-      width: "55px",
-      transform: "translate(-160px, 10px)",
+      width: "60px",
+      transform: "translate(0, 10px)",
     },
     text_style: {
-      transform: "translate(-140px, -10px)",
+      transform: "translate(20px, -10px)",
     },
     selected: false,
     div_style: {
-      left: "150px",
+      left: "-10px",
       top: "230px",
       width: "55px",
       height: "45px",
@@ -685,14 +684,13 @@ const teeth = [
     img: eight,
     style: {
       width: "50px",
-      transform: "translate(-160px, 0)",
     },
     text_style: {
-      transform: "translate(-130px, -25px)",
+      transform: "translate(30px, -25px)",
     },
     selected: false,
     div_style: {
-      left: "200px",
+      left: "40px",
       top: "220px",
       width: "55px",
       height: "45px",
@@ -703,14 +701,14 @@ const teeth = [
     img: nine,
     style: {
       width: "50px",
-      transform: "translate(-160px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-120px, -25px)",
+      transform: "translate(40px, -25px)",
     },
     selected: false,
     div_style: {
-      left: "250px",
+      left: "90px",
       top: "220px",
       width: "55px",
       height: "45px",
@@ -721,14 +719,14 @@ const teeth = [
     img: ten,
     style: {
       width: "60px",
-      transform: "translate(-160px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-100px, -20px)",
+      transform: "translate(70px, -20px)",
     },
     selected: false,
     div_style: {
-      left: "280px",
+      left: "125px",
       top: "235px",
       width: "55px",
       height: "45px",
@@ -739,14 +737,14 @@ const teeth = [
     img: eleven,
     style: {
       width: "60px",
-      transform: "translate(-140px, -5px)",
+      transform: "translate(-0, -5px)",
     },
     text_style: {
-      transform: "translate(-60px, -20px)",
+      transform: "translate(70px, -10px)",
     },
     selected: false,
     div_style: {
-      left: "290px",
+      left: "160px",
       top: "280px",
       width: "55px",
       height: "45px",
@@ -757,14 +755,14 @@ const teeth = [
     img: fifteen,
     style: {
       width: "60px",
-      transform: "translate(-100px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-10px, 15px)",
+      transform: "translate(75px, 15px)",
     },
     selected: false,
     div_style: {
-      left: "270px",
+      left: "180px",
       top: "320px",
       width: "55px",
       height: "45px",
@@ -775,14 +773,14 @@ const teeth = [
     img: sixteen,
     style: {
       width: "65px",
-      transform: "translate(-90px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(0, 10px)",
+      transform: "translate(80px, 20px)",
     },
     selected: false,
     div_style: {
-      left: "270px",
+      left: "200px",
       top: "390px",
       width: "55px",
       height: "45px",
@@ -793,14 +791,14 @@ const teeth = [
     img: thirtyone,
     style: {
       width: "60px",
-      transform: "translate(-155px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-150px, 35px)",
+      transform: "translate(-0, 35px)",
     },
     selected: false,
     div_style: {
-      left: "100px",
+      left: "-50px",
       top: "550px",
       height: "70px",
       width: "65px",
@@ -811,14 +809,14 @@ const teeth = [
     img: thirtytwo,
     style: {
       width: "60px",
-      transform: "translate(-165px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-165px, 25px)",
+      transform: "translate(5px, 25px)",
     },
     selected: false,
     div_style: {
-      left: "105px",
+      left: "-60px",
       top: "485px",
       width: "60px",
       height: "60px",
@@ -829,14 +827,15 @@ const teeth = [
     img: twentytwo,
     style: {
       width: "40px",
-      transform: "translate(-120px, -10px)",
+      transform: "translate(0, -10px)",
     },
     text_style: {
-      transform: "translate(-70px, 35px)",
+      transform: "translate(50px, 35px)",
     },
     selected: false,
     div_style: {
-      left: "300px",
+      border: "1px solid red",
+      left: "180px",
       top: "620px",
       width: "40px",
       height: "50px",
@@ -847,14 +846,14 @@ const teeth = [
     img: twentythree,
     style: {
       width: "40px",
-      transform: "translate(-118px, 5px)",
+      transform: "translate(0, 5px)",
     },
     text_style: {
-      transform: "translate(-80px, 60px)",
+      transform: "translate(40px, 60px)",
     },
     selected: false,
     div_style: {
-      left: "260px",
+      left: "140px",
       top: "630px",
       width: "40px",
       height: "50px",
@@ -865,14 +864,14 @@ const teeth = [
     img: twentyfour,
     style: {
       width: "40px",
-      transform: "translate(-128px, 10px)",
+      transform: "translate(0, 10px)",
     },
     text_style: {
-      transform: "translate(-100px, 70px)",
+      transform: "translate(30px, 70px)",
     },
     selected: false,
     div_style: {
-      left: "220px",
+      left: "100px",
       top: "640px",
       width: "40px",
       height: "50px",
@@ -883,14 +882,14 @@ const teeth = [
     img: twentyfive,
     style: {
       width: "40px",
-      transform: "translate(-128px, 10px)",
+      transform: "translate(0, 10px)",
     },
     text_style: {
-      transform: "translate(-100px, 70px)",
+      transform: "translate(30px, 70px)",
     },
     selected: false,
     div_style: {
-      left: "180px",
+      left: "55px",
       top: "640px",
       width: "40px",
       height: "50px",
@@ -901,14 +900,14 @@ const teeth = [
     img: twentysix,
     style: {
       width: "40px",
-      transform: "translate(-137px, 10px)",
+      transform: "translate(0, 10px)",
     },
     text_style: {
-      transform: "translate(-115px, 60px)",
+      transform: "translate(15px, 60px)",
     },
     selected: false,
     div_style: {
-      left: "150px",
+      left: "10px",
       top: "630px",
       width: "40px",
       height: "50px",
@@ -919,14 +918,14 @@ const teeth = [
     img: twentyseven,
     style: {
       width: "40px",
-      transform: "translate(-135px, -10px)",
+      transform: "translate(0, -10px)",
     },
     text_style: {
-      transform: "translate(-130px, 30px)",
+      transform: "translate(5px, 25px)",
     },
     selected: false,
     div_style: {
-      left: "110px",
+      left: "-20px",
       top: "620px",
       width: "50px",
       height: "50px",
@@ -937,14 +936,14 @@ const teeth = [
     img: seventeen,
     style: {
       width: "65px",
-      transform: "translate(-90px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-10px, 20px)",
+      transform: "translate(80px, 20px)",
     },
     selected: false,
     div_style: {
-      left: "290px",
+      left: "200px",
       top: "490px",
       width: "40px",
       height: "50px",
@@ -955,14 +954,14 @@ const teeth = [
     img: eighteen,
     style: {
       width: "60px",
-      transform: "translate(-90px, 0)",
+      transform: "translate(0, 0)",
     },
     text_style: {
-      transform: "translate(-15px, 40px)",
+      transform: "translate(70px, 30px)",
     },
     selected: false,
     div_style: {
-      left: "280px",
+      left: "190px",
       top: "560px",
       width: "40px",
       height: "50px",
